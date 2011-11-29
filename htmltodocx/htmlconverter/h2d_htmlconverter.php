815c4f4ada766d9a38fad94192eb76065e44e4cc
neilt17 <none@none.com>
2011-11-29 20:03:37 +0000

    Mapping out inline style conversion process.

--- htmltodocx/htmlconverter/h2d_htmlconverter.php
+++ htmltodocx/htmlconverter/h2d_htmlconverter.php
@@ -160,7 +160,7 @@ function h2d_get_style($element, $state) {
   // for size and colours. The idea would be able to have a style like:
   // array('color: #{hex}' => array ('color' => '{hex}')); or
   // array('border-width: {px}' => array ('borderSize' => '{twip}'));
-  // in the latter case, this would trigger a conversion from px to twips
+  // in the latter case, this would trigger a conversion from px to twips (SEE BELOW)
   
   // Find any inline styles:
   $inline_style_list = array();
@@ -485,3 +485,393 @@ function h2d_insert_html(&$phpword_element, $html_dom_array, &$state = array()) 
     $state['current_style'] = $old_style;
   }
 }
+
+// Processing inline styles:
+
+// What type of variable have we got:
+// Somethings can have one to four numerical values - specifying 
+// say left, right, top bottom - for colours and measurements;
+// Others have variable number of values - e.g. font size, colour, font style:
+// IDENTIFYING WHAT WE HAVE GOT:
+// =============================
+
+/*
+NB, there are no "shorthand" styles in PHPWord - all one to one attribute-values
+
+Allow custom overriding of the values converter
+
+
+
+
+
+'#multi' for 1 to 4 way order dependent settings
+you should provide all four values however.
+
+<attribute-name> '#box_ordered' => TRUE if describe attributes around a box and order is important (or FALSE if not important);
+                 '0' => 'type' = 'hex' // options: hex, twip, pt, px, // you must use the correct PHPWord type for this element, otherwise you will get strange results! (e.g. very large or very small)
+                                      //text,
+                                      // position, list_style_type
+                                      
+                        'html_value' = 'inline' (say) - only required if type = 'text'
+                        'phpword_name' = PHPAttribute name
+                        'phpword_value' = PHPAttribute value // for type text only - otherwise
+                        this value is created'
+                 '1' => 'type' = 'color' .... etc.
+ 
+        attribute:        
+tag:    width   height    color   font-size   other
+img     px      px      
+other   twip    twip      hex     pt          twip
+
+*/
+
+
+function h2d_inline_styles() {
+  
+  $inline_styles = array(
+    'border' => array(
+      '#box_ordered' => FALSE,
+      0 => array(
+        'type' => 'twip',
+        'phpword' => array(
+          'borderSize' => '', // will be replaced with the appropriate value
+        ),
+      ),
+      // NB border style can't be set in PHPWord
+      1 => array(
+        'type' => 'hex',
+        'phpword' => array(
+          'borderColor' => '', // will be replaced with the appropriate value
+        ),
+      ),
+    ),
+    
+    'border-width' => array(
+      '#box_ordered' => TRUE, // If you don't specify less than four measurements
+                              // there may be circumstances when a css measurement
+                              // is not applied.
+      0 => array(
+        'type' => 'twip',     // note borders can only be defined in twips
+        'phpword' => array(
+          'borderTopSize' => '',
+        ),
+      ),
+      1 => array(
+        'type' => 'twip',
+        'phpword' => array(
+          'borderRightSize' => '',
+        ),
+      ),
+      2 => array(
+        'type' => 'twip',
+        'phpword' => array(
+          'borderBottomSize' => '',
+        ),
+      ),
+      3 => array(
+        'type' => 'twip',
+        'phpword' => array(
+          'borderLeftSize' => '',
+        ),
+      ),
+    ),
+    
+    'text-decoration' => array(
+      '#box_ordered' => FALSE,
+      0 => array(
+        'type' => 'text', // default - don't need to specify
+        'css_value' => 'underline',
+        'phpword' => array(
+          'underline'=>PHPWord_Style_Font::UNDERLINE_SINGLE,
+        ),
+      ),
+      1 => array(
+        'type' => 'text', // default - don't need to specify
+        'css_value' => 'none',
+        'phpword' => array(
+          'underline' => 'none', // signals that this phpword attribute should be unset
+        ),
+      ),
+    ),
+    
+    
+    
+    
+    
+  ); 
+  
+  
+  
+  
+}
+
+
+/* 
+                  
+if order_important is set to true, then if four values map:
+0->0, 1->1, 2->2, 3->3;
+three values:
+0->0, 1->1, 1->3, 2->2;
+two values:
+0->0, 0->2, 1->1, 1->3;
+one value:
+0->0, 0->1, 0->2, 0->3;
+
+a value of auto - counts as a value, but not supported - so will be ignored (no computable widths of elements to be able to calculate what the margins should be).
+
+// identify what certain numerical values are, 
+// everything else is "text" - needing a 1:1 substitution:
+
+// colour (hex)
+#([0-9a-fA-F]{3}){1,2}
+// colour (red, green, blue etc.)
+convert to the appropriate numerical value
+
+backgrounds with url() - not supported;
+
+position constants - convert to appropriate PHPWord position:
+top, center, bottom;
+left, center, right;
+justify (equiv to justify == both in PHPWord)
+
+also (css): baseline, middle, sub, super, text-bottom, text-top.
+
+// per cent
+<numerical value> followed by '%' (is percentage of current style for this parameter)
+
+similarly for em and ex: 1.25em or 1.25ex is 1.25x the currently set size
+
+// pixel
+<numerical value> folowed by 'px'
+
+similarly - in, cm, mm, pc (as well as pt px)
+pc - 1/6 inch
+// zero value
+0 
+
+// border style - not supported in PHPWord
+
+// List-style-type - can be supported for pseudo lists (but not PHPWord lists:
+circle
+disc
+square
+lower-alpha
+upper-alpha
+lower-roman
+upper-roman
+none
+
+// text width:
+thin, medium or thick - convert to 1, 3 and 5px (this is the standard)
+
+// Other tags to include:
+code;
+
+// page-break-after/page-break-before (p417 css book)
+- works only if set to always
+- add a page break before or after the element (needs to go in the converter).
+
+
+// for headings - addTitleStyle - when you come to a heading;
+// then addTitle for the heading (text for the title, depth)
+// note - you cannot do any more styling with headings created in this way - so any 
+child elements will be all returned as text (when a heading is encountered)
+// switch this feature on or off;
+// with this feature, insert a table of contents at the beginning - separate
+// styling for this. Specify an element for this to appear after (or none). e.g.
+// 'h1' - this will be inserted after the first h1 element and so on.
+
+text-transform - if set (feature within the converter) :
+to capitalize, lowercase, uppercase (or none) the contained text.
+
+another feature:
+display - skip the element (value - none) - other values meaning display it
+visibility - set the text in the element to '' (value - hidden) 
+
+another feature for the converter:
+whitespace - normal - as we have it;
+nowrap - the same as above - we can't have endless text lines in word;
+pre - whitespace is not collapsed (but could still break to a new line) (same for <pre> tag)
+
+// You can't flow text around images
+
+// font - style, - text match; variant - text match, weight - text match, (any order - optional) followed by size (required) - number match [line-height] family - text match
+// so for font we can actually specify order NOT important
+
+// font: constant - just a text replacement thing
+
+// absolute size constants - for font size:
+// 
+array('xx-small' => '6',
+'x-small' => '8',
+'small' => '10',
+'medium' => '12',
+'large' => '14',
+'x-large' => '18',
+'xx-large' => '24',);
+
+'smaller' => 1/1.2 of existing size
+'larger' => 1.2 x existing size
+
+// points
+<numeric value> followed by 'pt'
+
+font-style - simple text replacement
+
+numeric value = 
+a number - for font-weight - not supported - values are bold (or not)
+
+*/
+// Colour names converter:
+// From: http://www.w3schools.com/cssref/css_colornames.asp
+$colours = array(
+'AliceBlue' => '#F0F8FF',
+'AntiqueWhite' => '#FAEBD7',
+'Aqua' => '#00FFFF',
+'Aquamarine' => '#7FFFD4',
+'Azure' => '#F0FFFF',
+'Beige' => '#F5F5DC',
+'Bisque' => '#FFE4C4',
+'Black' => '#000000',
+'BlanchedAlmond' => '#FFEBCD',
+'Blue' => '#0000FF',
+'BlueViolet' => '#8A2BE2',
+'Brown' => '#A52A2A',
+'BurlyWood' => '#DEB887',
+'CadetBlue' => '#5F9EA0',
+'Chartreuse' => '#7FFF00',
+'Chocolate' => '#D2691E',
+'Coral' => '#FF7F50',
+'CornflowerBlue' => '#6495ED',
+'Cornsilk' => '#FFF8DC',
+'Crimson' => '#DC143C',
+'Cyan' => '#00FFFF',
+'DarkBlue' => '#00008B',
+'DarkCyan' => '#008B8B',
+'DarkGoldenRod' => '#B8860B',
+'DarkGray' => '#A9A9A9',
+'DarkGrey' => '#A9A9A9',
+'DarkGreen' => '#006400',
+'DarkKhaki' => '#BDB76B',
+'DarkMagenta' => '#8B008B',
+'DarkOliveGreen' => '#556B2F',
+'Darkorange' => '#FF8C00',
+'DarkOrchid' => '#9932CC',
+'DarkRed' => '#8B0000',
+'DarkSalmon' => '#E9967A',
+'DarkSeaGreen' => '#8FBC8F',
+'DarkSlateBlue' => '#483D8B',
+'DarkSlateGray' => '#2F4F4F',
+'DarkSlateGrey' => '#2F4F4F',
+'DarkTurquoise' => '#00CED1',
+'DarkViolet' => '#9400D3',
+'DeepPink' => '#FF1493',
+'DeepSkyBlue' => '#00BFFF',
+'DimGray' => '#696969',
+'DimGrey' => '#696969',
+'DodgerBlue' => '#1E90FF',
+'FireBrick' => '#B22222',
+'FloralWhite' => '#FFFAF0',
+'ForestGreen' => '#228B22',
+'Fuchsia' => '#FF00FF',
+'Gainsboro' => '#DCDCDC',
+'GhostWhite' => '#F8F8FF',
+'Gold' => '#FFD700',
+'GoldenRod' => '#DAA520',
+'Gray' => '#808080',
+'Grey' => '#808080',
+'Green' => '#008000',
+'GreenYellow' => '#ADFF2F',
+'HoneyDew' => '#F0FFF0',
+'HotPink' => '#FF69B4',
+'IndianRed ' => '#CD5C5C',
+'Indigo ' => '#4B0082',
+'Ivory' => '#FFFFF0',
+'Khaki' => '#F0E68C',
+'Lavender' => '#E6E6FA',
+'LavenderBlush' => '#FFF0F5',
+'LawnGreen' => '#7CFC00',
+'LemonChiffon' => '#FFFACD',
+'LightBlue' => '#ADD8E6',
+'LightCoral' => '#F08080',
+'LightCyan' => '#E0FFFF',
+'LightGoldenRodYellow' => '#FAFAD2',
+'LightGray' => '#D3D3D3',
+'LightGrey' => '#D3D3D3',
+'LightGreen' => '#90EE90',
+'LightPink' => '#FFB6C1',
+'LightSalmon' => '#FFA07A',
+'LightSeaGreen' => '#20B2AA',
+'LightSkyBlue' => '#87CEFA',
+'LightSlateGray' => '#778899',
+'LightSlateGrey' => '#778899',
+'LightSteelBlue' => '#B0C4DE',
+'LightYellow' => '#FFFFE0',
+'Lime' => '#00FF00',
+'LimeGreen' => '#32CD32',
+'Linen' => '#FAF0E6',
+'Magenta' => '#FF00FF',
+'Maroon' => '#800000',
+'MediumAquaMarine' => '#66CDAA',
+'MediumBlue' => '#0000CD',
+'MediumOrchid' => '#BA55D3',
+'MediumPurple' => '#9370D8',
+'MediumSeaGreen' => '#3CB371',
+'MediumSlateBlue' => '#7B68EE',
+'MediumSpringGreen' => '#00FA9A',
+'MediumTurquoise' => '#48D1CC',
+'MediumVioletRed' => '#C71585',
+'MidnightBlue' => '#191970',
+'MintCream' => '#F5FFFA',
+'MistyRose' => '#FFE4E1',
+'Moccasin' => '#FFE4B5',
+'NavajoWhite' => '#FFDEAD',
+'Navy' => '#000080',
+'OldLace' => '#FDF5E6',
+'Olive' => '#808000',
+'OliveDrab' => '#6B8E23',
+'Orange' => '#FFA500',
+'OrangeRed' => '#FF4500',
+'Orchid' => '#DA70D6',
+'PaleGoldenRod' => '#EEE8AA',
+'PaleGreen' => '#98FB98',
+'PaleTurquoise' => '#AFEEEE',
+'PaleVioletRed' => '#D87093',
+'PapayaWhip' => '#FFEFD5',
+'PeachPuff' => '#FFDAB9',
+'Peru' => '#CD853F',
+'Pink' => '#FFC0CB',
+'Plum' => '#DDA0DD',
+'PowderBlue' => '#B0E0E6',
+'Purple' => '#800080',
+'Red' => '#FF0000',
+'RosyBrown' => '#BC8F8F',
+'RoyalBlue' => '#4169E1',
+'SaddleBrown' => '#8B4513',
+'Salmon' => '#FA8072',
+'SandyBrown' => '#F4A460',
+'SeaGreen' => '#2E8B57',
+'SeaShell' => '#FFF5EE',
+'Sienna' => '#A0522D',
+'Silver' => '#C0C0C0',
+'SkyBlue' => '#87CEEB',
+'SlateBlue' => '#6A5ACD',
+'SlateGray' => '#708090',
+'SlateGrey' => '#708090',
+'Snow' => '#FFFAFA',
+'SpringGreen' => '#00FF7F',
+'SteelBlue' => '#4682B4',
+'Tan' => '#D2B48C',
+'Teal' => '#008080',
+'Thistle' => '#D8BFD8',
+'Tomato' => '#FF6347',
+'Turquoise' => '#40E0D0',
+'Violet' => '#EE82EE',
+'Wheat' => '#F5DEB3',
+'White' => '#FFFFFF',
+'WhiteSmoke' => '#F5F5F5',
+'Yellow' => '#FFFF00',
+'YellowGreen' => '#9ACD32',
+);
+
+
